45b9c97cadc0a4733f3855b6b544ae712c112f31
Maschkov Konstantin <maschkovc@yandex.ru>
2023-11-28 16:33:38 +0300

    регистрация + хеширование пароля

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -6,7 +6,9 @@ const handler = NextAuth({
 	providers: [
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
-			clientSecret: process.env.GOOGLE_CLIENT_SECRETT,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+			// clientId: "135522685600-kvced9ru0e7hl32thpl4agfcstlffebb.apps.googleusercontent.com",
+			// clientSecret: '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',
 		}),
 	],
 })
--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import styles from './page.module.scss';
+"use client";
+import React, { useState } from "react";
+import styles from "./page.module.scss";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+	const [error, setError] = useState(null);
 
-export default Register
+	const router = useRouter();
+
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		const name = e.target[0].value;
+		const email = e.target[1].value;
+		const password = e.target[2].value;
+
+		try {
+			const res = await fetch("/api/auth/register", {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify({
+					name,
+					email,
+					password,
+				}),
+			});
+			res.status === 201 &&
+				router.push("/dashboard/login?success=Account has been created");
+		} catch (err) {
+			setError(err);
+			console.log(err);
+		}
+	};
+
+	return (
+		<div className={styles.container}>
+			<h2 className={styles.title}>Создать учетную <br />запись</h2>
+			<form onSubmit={handleSubmit} className={styles.form}>
+				<input
+					type="text"
+					placeholder="Ваше имя"
+					required
+					className={styles.input}
+				/>
+				<input
+					type="text"
+					placeholder="Ваш E-mail"
+					required
+					className={styles.input}
+				/>
+				<input
+					type="password"
+					placeholder="Придумайте пароль"
+					required
+					className={styles.input}
+				/>
+				<button className={styles.button}>Зарегистрироваться</button>
+				{error && "Something went wrong!"}
+			</form>
+			<span className={styles.or}><h3>- или -</h3></span>
+			<Link className={styles.link} href="/dashboard/login">
+				<h3>Войдите с существующей <br /> учетной записью</h3>
+			</Link>
+		</div>
+	);
+};
+
+export default Register;
